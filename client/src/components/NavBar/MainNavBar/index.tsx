cac1e51a1d94cc171aa1f92f39ad89f8b7d7b8eb
aryantmg628 <aryantmg628@gmail.com>
2025-06-01 20:13:15 +0545

    Feat: Shadcn ui components added

--- client/src/components/NavBar/MainNavBar/index.tsx
+++ client/src/components/NavBar/MainNavBar/index.tsx
@@ -1,13 +1,15 @@
 import { Logo } from "@/components/common/Logo";
+import { ThemeToggle } from "@/components/common/ThemeToggle";
 import { UserButton } from "@clerk/nextjs";
 
 export const MainNavBar = () => {
   return (
-    <header className="flex items-center justify-between">
+    <header className="flex items-center justify-between ">
       <div>
         <Logo />
       </div>
-      <div>
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
         <UserButton
           appearance={{
             elements: {
