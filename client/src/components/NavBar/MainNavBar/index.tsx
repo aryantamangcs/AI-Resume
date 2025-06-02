ed6ee919fc6b23941b50ea263ea8d12d77df3168
aryantmg628 <aryantmg628@gmail.com>
2025-06-02 18:52:54 +0545

    refactor : refactor the design

--- client/src/components/NavBar/MainNavBar/index.tsx
+++ client/src/components/NavBar/MainNavBar/index.tsx
@@ -1,26 +1,33 @@
+"use client";
 import { Logo } from "@/components/common/Logo";
 import { ThemeToggle } from "@/components/common/ThemeToggle";
 import { UserButton } from "@clerk/nextjs";
+import { useTheme } from "next-themes";
+import { dark } from "@clerk/themes";
 
 export const MainNavBar = () => {
+  const { theme } = useTheme();
   return (
-    <header className="flex items-center justify-between ">
-      <div>
-        <Logo />
-      </div>
-      <div className="flex items-center gap-4">
-        <ThemeToggle />
-        <UserButton
-          appearance={{
-            elements: {
-              avatarBox: {
-                width: 35,
-                height: 35,
+    <div className="w-screen px-4 py-1 shadow-sm flex justify-center">
+      <header className="flex items-center justify-between w-screen max-w-[1000px]">
+        <div>
+          <Logo />
+        </div>
+        <div className="flex items-center gap-4">
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: {
+                avatarBox: {
+                  width: 35,
+                  height: 35,
+                },
               },
-            },
-          }}
-        />
-      </div>
-    </header>
+            }}
+          />
+        </div>
+      </header>
+    </div>
   );
 };
