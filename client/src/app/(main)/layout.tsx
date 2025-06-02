55a19b9b2eec90968d842816ddc23b1daba7566c
aryantmg628 <aryantmg628@gmail.com>
2025-06-02 18:53:24 +0545

    refactor : refactor the design

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 import React from "react";
 const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="p-4">
+    <div>
       <MainNavBar />
       {children}
     </div>
